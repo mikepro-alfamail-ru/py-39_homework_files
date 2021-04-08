2b9c552ab8d4ed57c87dc19e344604e8e07e8960
Mikhail Prozorov <mikepro@alfamail.ru>
2021-04-08 12:24:56 +0300

    Add file_to_cookdict_opt function

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pprint
-
 def is_int(string):
     try:
         result = int(string)
@@ -8,58 +6,112 @@ def is_int(string):
         return False
 
 #Задание 1, вариант 1
-def file_to_coockdict_readline(file):
+def file_to_cookdict_readline(file):
     '''file - строка. Эта функция читает файл построчно.
     Окончание чтения проверяется по пустой строке'''
-    coock_dict = {}
+    cook_dict = {}
     with open(file, 'rt', encoding='utf-8') as f:
         eof = False
         while not eof:
             dish = f.readline().strip()
             if dish != '':
-                coock_dict[dish] = []
+                cook_dict[dish] = []
                 n = int(f.readline())
                 for i in range(n):
                     ingridient = f.readline().strip().split(' | ')
-                    coock_dict[dish].append({'ingredient_name': ingridient[0], 'quantity': int(ingridient[1]), 'measure': ingridient[2]})
+                    cook_dict[dish].append(
+                        {
+                            'ingredient_name': ingridient[0],
+                            'quantity': int(ingridient[1]),
+                            'measure': ingridient[2]
+                        }
+                    )
                 a = f.readline().strip()
             else:
                 eof = True
-    return coock_dict
+    return cook_dict
 
 #Задание 1, вариант 2
-def file_to_coockdict(file):
+def file_to_cookdict(file):
     '''Эта функция считывает файл целиком. В этом случае нет необходимости писать количество ингридиентов. Переход к
     следующему рецепту осуществляется по пустой строке.'''
-    coock_dict = {}
+    cook_dict = {}
     with open(file, 'rt', encoding='utf-8') as f:
-        coock_book = f.readlines()
-    for line in coock_book:
+        cook_book = f.readlines()
+    for line in cook_book:
         if line.strip() != '':
             if '|' not in line.strip():
                if not is_int(line.strip()):
                    dish = line.strip()
-                   coock_dict[dish] = []
+                   cook_dict[dish] = []
             else:
                 ingridient = line.strip().split(' | ')
-                coock_dict[dish].append({'ingredient_name': ingridient[0], 'quantity': int(ingridient[1]), 'measure': ingridient[2]})
-    return coock_dict
+                cook_dict[dish].append(
+                    {
+                        'ingredient_name': ingridient[0],
+                        'quantity': int(ingridient[1]),
+                        'measure': ingridient[2]
+                    }
+                )
+    return cook_dict
 
+def file_to_cookdict_opt(file):
+    cook_dict = {}
+    with open(file, 'r', encoding='utf8') as f:
+        for line in f:
+            cook_dict[line.strip()] = []
+            count = int(f.readline())
+            for _ in range(count):
+                ingredient = f.readline().strip().split(' | ')
+                cook_dict[line.strip()].append(
+                    {
+                        'ingredient_name': ingredient[0],
+                        'quantity': int(ingredient[1]),
+                        'measure': ingredient[2]
+                    }
+                )
+            f.readline()
+    return cook_dict
 
 #Задание 2
-def get_shop_list_by_dishes(dishes, person_count, coock_dict):
+def get_shop_list_by_dishes(dishes, person_count, cook_dict):
     shop_list = {}
     for dish in dishes:
-        for ingredient in coock_dict[dish]:
+        for ingredient in cook_dict[dish]:
             if ingredient['ingredient_name'] in shop_list:
                 shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
             else:
-                shop_list[ingredient['ingredient_name']] = {'quantity': ingredient['quantity'] * person_count, 'measure': ingredient['measure']}
+                shop_list[ingredient['ingredient_name']] = \
+                    {
+                        'quantity': ingredient['quantity'] * person_count,
+                        'measure': ingredient['measure']
+                    }
     return shop_list
 
-coock_dict = file_to_coockdict_readline('recipes.txt')
+cook_dict = file_to_cookdict_readline('recipes.txt')
 
-pprint.pprint(coock_dict)
-pprint.pprint(file_to_coockdict('recipes.txt'))
-pprint.pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, coock_dict))
-pprint.pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, file_to_coockdict('recipes.txt')))
+data = file_to_cookdict_opt('recipes.txt')
+
+with open('recipes1.txt', 'w', encoding='utf8') as f:
+    for dish, ingredients in data.items():
+        f.write(dish)
+        f.write('\n')
+        f.write(str(len(ingredients)))
+        f.write('\n')
+        for ingredient in ingredients:
+            f.write(
+                ingredient['ingredient_name'] +
+                ' | ' +
+                str(ingredient['quantity']) +
+                ' | ' +
+                ingredient['measure'] + '\n'
+            )
+
+        f.write('\n')
+
+
+#
+# pprint.pprint(cook_dict)
+# pprint.pprint(file_to_cookdict('recipes.txt'))
+# pprint.pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, cook_dict))
+# pprint.pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, file_to_cookdict('recipes.txt')))
